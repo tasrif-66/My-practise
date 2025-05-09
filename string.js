cc8f093e20b82df9f148757a8c30aa3a8d2373da
tasrif-66 <mtasrif20@gmail.com>
2025-05-10 01:15:36 +0600

    string practise

---
+++ string.js
@@ -0,0 +1,25 @@
+const name = "Md Abdullah Tasrif";
+console.log(name);
+
+const address = "mannan vella ,kobir housing socity,Hamzar bug , chattagram";
+console.log(address);
+
+const fraiensUniversity = "";
+console.log(fraiensUniversity);
+
+const fan = "Forhad Mustahfa";
+console.log(fan);
+
+const movieOFTheYear = "Aura";
+console.log(movieOFTheYear);
+
+const hobby = "traviling";
+console.log(hobby);
+
+const color = "black and white";
+console.log(color);
+
+
+ 
+
+
