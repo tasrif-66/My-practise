795f91deb6dee74b959a19720af8a15e457778b7
tasrif-66 <mtasrif20@gmail.com>
2025-05-09 21:58:14 +0600

    Initial commit

---
+++ log.js
@@ -0,0 +1,20 @@
+const age = 26;
+console.log(age);
+
+const booksToRead = 25;
+console.log(booksToRead);
+
+const wakeUpTime = 5;
+console.log(wakeUpTime);
+
+const wasteTime = 6;
+console.log(wakeUpTime);
+
+const borrow = 2;
+console.log(borrow);
+
+const golas = 7;
+console.log(golas);
+
+const pillow = 2;
+console.log(pillow);
